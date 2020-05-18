66817238621a26bf09bac2766eb452beba9a3a56
Tobias Iven <tobiasiven@gmail.com>
2020-05-17 21:07:25 -0700

    Iter 500000 lr .000001 Any other combo either produced nans, moved too slowly, or took too long.

--- R/linear_regression_alt.R
+++ R/linear_regression_alt.R
@@ -11,40 +11,66 @@
 #' @export
 slr_gd <- function(dat, response, explanatory){
 
+
   ### Compute coefficients by gradient descent
   ### Return a data frame of the same form as in the `simple_linear_regression`
 
-  x <- dat %>% pull({{explanatory}})
-  y <- dat %>% pull({{response}})
+  x <- dat %>% select({{explanatory}})
+  y <- dat %>% select({{response}})
 
-  x <- cbind(Intercept = 1, x)
+  xnames <- c("Intercept", names(x))
 
-  lr <- .0015
+  x <- as.matrix(cbind(Intercept = 1, x))
 
-  gdc = as.matrix(c(0,0))
+  lr <- .000001
+
+  gdc = as.matrix(c(0, 0))
 
   n = length(y)
 
   error <- sum((x %*% gdc - y))^2/n
 
-  for (i in 1:20000){
+  nrep = 500000
 
+  checker = nrep/100
+
+  # slops = rep(0, 100)
+  # ints = rep(0, 100)
+  # errs = rep(0, 100)
+
+  for (i in 1:nrep){
     preds = x %*% gdc
-    diff = y - preds
+    differ = y - preds
 
-    DM <- -2 * sum(x[,2] * diff) / n
+    if (i %% checker == 0){
+      print(paste(i * 100 /nrep,"% Done"))
+
+      # store = i/checker
+      # slops[store] = gdc[2]
+      # ints[store] = gdc[1]
+      # errs[store] = sum(differ^2)/n
+      }
+
+    DM <- -2 * sum(x[,2] * (differ)) / n
     gdc[2] <- gdc[2] - lr * DM
 
-    DB <- -2 * sum(diff) / n
+
+    DB <- -2 * sum(differ) / n
     gdc[1] <- gdc[1] - lr * DB
 
-    error <- sum(diff^2)/n
   }
 
-  print(gdc)
+  results <- data.frame(t(gdc))
+  names(results) <- xnames
+
+  # plot(slops, main = "Slopes", xlab = "Iteration in 10,000s", ylab = "Slope")
+  # plot(ints, main = "Intercepts", xlab = "Iteration in 10,000s", ylab = "Intercept")
+  # plot(errs, main = "Error", xlab = "Iteration in 10,000s", ylab = "Error")
+
   return(results)
 
 }
+
 
 
 #' Implements linear regression with many predictors by gradient descent
@@ -64,32 +90,46 @@ slr_gd <- function(dat, response, explanatory){
 #' @import dplyr
 #'
 #'@export
+#'
+#'
+
+
+
 mlr_gd <- function(dat, response) {
+  lr = .000001
+  nrep = 500000
+
+  checker = nrep/100
 
   x <- dat %>% select(-{{response}})
   y <- dat %>% select({{response}})
 
-  xnames <- names(x)
+  n <- length(y)
 
-  x <- as.matrix(x)
-  y <- as.matrix(y)
+  xnames <- c("Intercept", names(x))
 
-  x <- cbind(Intercept = 1, x)
-
-  lr <- .0015
+  x <- as.matrix(cbind(Intercept = 1,x))
 
   nx <- ncol(x)
+  xnames <- names(x)
 
-  gdc <- matrix(0, nx + 1)
+  gdc <- matrix(0, nx)
+  print(gdc)
+  print(x)
 
-  while (error > 1.01 * rer){
+
+  for (i in 1:nrep){
+
+    if (i %% checker == 0){
+      print(paste(i * 100 /nrep,"% Done"))
+    }
 
     preds = x %*% gdc
     diff = y - preds
 
-    for (i in 1:nx){
-      DM <- -2 * sum(x[,i+1] * diff) / n
-      gdc[i+1] <- gdc[i+1] - lr * DM
+    for (j in 2:nx){
+      DM <- -2 * sum(x[,j] * diff) / n
+      gdc[j] <- gdc[j] - lr * DM
 
     }
 
@@ -97,7 +137,12 @@ mlr_gd <- function(dat, response) {
     gdc[1] <- gdc[1] - lr * DB
 
     error <- sum(diff^2)/n
+
   }
+
+
+  results <- data.frame(t(gdc))
+  names(results) <- xnames
 
   return(results)
 
@@ -111,10 +156,9 @@ mlr_gd <- function(dat, response) {
 #'
 #' @return A vector: the projection of u onto a
 #'
+#'@export
 
 proj <- function(u, a){
-  #print(u)
-  #print(a)
   return(as.numeric(((t(a) %*% u) / (t(u) %*% u))) %*% u)
 }
 
@@ -127,7 +171,7 @@ proj <- function(u, a){
 #'
 #' @return a list of the q and r matrices
 #'
-#'
+#' @export
 
 
 gramsch <- function(x){
@@ -181,6 +225,9 @@ gramsch <- function(x){
 #' @import dplyr
 #'
 #'@export
+#'
+
+
 mlr_qr <- function(dat, response) {
   x <- dat %>% select(-{{response}})
   y <- dat %>% select({{response}})
